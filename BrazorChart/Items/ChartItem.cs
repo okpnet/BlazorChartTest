35d0723ab38aa5b59332d2c2fafaf80582646f42
umai-beer <umai-beer@hotmail.co.jp>
2024-08-13 21:55:13 +0900

    ok

---
+++ BrazorChart/Items/ChartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrazorChart.Items
+{
+    public class ChartItem
+    {
+        public string Name { get; set; }
+
+        public decimal Value { get; set; }
+    }
+}
---
+++ BrazorChart/Items/ChartItemFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrazorChart.Items
+{
+    public static class ChartItemFactory
+    {
+        public static ICollection<ChartItem> Create()
+        {
+            return 
+                [
+                    new() { Name = "A", Value = 5 },
+                    new() {Name = "B", Value = 6 },
+                    new(){Name = "C", Value = 1 },
+                    new(){Name="D", Value = 4 },
+                ];
+        }
+    }
+}
